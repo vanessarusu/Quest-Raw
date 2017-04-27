5438403ef1fd44dbcb71a718d34e751c505e8000
Vanessa Rusu <vanessarusu@gmail.com>
2017-04-27 13:30:09 -0700

    Nav Fix

--- js/navigation.js
+++ js/navigation.js
@@ -39,7 +39,7 @@ for(var i=0; i<links.length; i++)
     console.log('for');
     href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.nodeValue.toString();
     // if(href !== null && href.length > 1 && href.substr(0, 1) == '#')
-    if(href !== null && href.length > 1 && href.indexOf("/#") > -1)
+    if(href !== null && href.length > 1 && href.indexOf("#") > -1)
     {
         console.log('hi :)');
         links[i].onclick = function()
