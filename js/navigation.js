364778f304d64bbdf6aaca6e399040899530d6a7
Vanessa Rusu <vanessarusu@gmail.com>
2017-04-27 13:55:33 -0700

    remove smooth scroll

--- js/navigation.js
+++ js/navigation.js
@@ -28,60 +28,63 @@
 
 // Smooth Scroll
 
-(function()
-{
-var speed = 300;
-var moving_frequency = 15; // Affects performance !
-var links = document.getElementsByTagName('a');
-var href;
-for(var i=0; i<links.length; i++)
-{   
+// (function()
+// {
+// var speed = 300;
+// var moving_frequency = 15; // Affects performance !
+// var links = document.getElementsByTagName('a');
+// var href;
+// for(var i=0; i<links.length; i++)
+// {   
     
-    href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.nodeValue.toString();
-    // if(href !== null && href.length > 1 && href.substr(0, 1) == '#')
-    if(href !== null && href.length > 1 && href.indexOf("#") > -1)
-    {
-        // console.log('hi :)');
-        links[i].onclick = function()
-        {
-            var element;
-            var href = this.attributes.href.nodeValue.toString();
-            if(element = document.getElementById(href.substr(1)))
-            {
-                var hop_count = speed/moving_frequency
-                var getScrollTopDocumentAtBegin = getScrollTopDocument();
-                var gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / hop_count;
+//     href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.nodeValue.toString();
+//     if(href !== null && href.length > 1 && href.substr(0, 1) == '#')
+//     // if(href !== null && href.length > 1 && href.indexOf("#") > -1)
+//     {
+//         // console.log('hi :)');
+//         links[i].onclick = function()
+//         {
+//             var element;
+//             var href = this.attributes.href.nodeValue.toString();
+//             if(element = document.getElementById(href.substr(1)))
+//             {
+//                 var hop_count = speed/moving_frequency
+//                 var getScrollTopDocumentAtBegin = getScrollTopDocument();
+//                 var gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / hop_count;
 
-                for(var i = 1; i <= hop_count; i++)
-                {
-                    (function()
-                    {
-                        var hop_top_position = gap*i;
-                        setTimeout(function(){  window.scrollTo(0, hop_top_position + getScrollTopDocumentAtBegin); }, moving_frequency*i);
-                    })();
-                }
-            }
+//                 for(var i = 1; i <= hop_count; i++)
+//                 {
+//                     (function()
+//                     {
+//                         var hop_top_position = gap*i;
+//                         setTimeout(function(){  window.scrollTo(0, hop_top_position + getScrollTopDocumentAtBegin); }, moving_frequency*i);
+//                     })();
+//                 }
+//             }
 
-            return false;
-        };
-    }
-}
+//             return false;
+//         };
+//     }
+// }
 
-var getScrollTopElement =  function (e)
-{
-    var top = 0;
+// var getScrollTopElement =  function (e)
+// {
+//     var top = 0;
 
-    while (e.offsetParent != undefined && e.offsetParent != null)
-    {
-        top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
-        e = e.offsetParent;
-    }
+//     while (e.offsetParent != undefined && e.offsetParent != null)
+//     {
+//         top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+//         e = e.offsetParent;
+//     }
 
-    return top;
-};
+//     return top;
+// };
 
-var getScrollTopDocument = function()
-{
-    return document.documentElement.scrollTop + document.body.scrollTop;
-};
-})();
+// var getScrollTopDocument = function()
+// {
+//     return document.documentElement.scrollTop + document.body.scrollTop;
+// };
+// })();
+
+
+
